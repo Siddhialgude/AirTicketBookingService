7e9af68bb82eccf37dd9eea00c8e193b3eb649b0
siddhi Algude <algudesiddhi04@gmail.com>
2024-03-13 00:16:41 +0530

    added error files

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const {StatusCodes} =  require('http-status-codes');
+
+class ServiceError extends Error {
+  constructor(
+    message='something went wrong',
+    explaination='service layer error',
+    statusCode= StatusCodes.INTERNAL_SERVER_ERROR
+  ){
+    this.name = 'ServiceError';
+    this.message =message;
+    this.explaination =explaination;
+    this.statusCode =statusCode
+
+  }
+}
+module.exports = ServiceError;
---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,22 @@
+const {StatusCodes} =require('http-status-codes');
+
+
+
+class ValidationError extends Error{
+  constructor(error){
+    super( );
+    let explaination = [];
+
+    error.errors.forEach((err) => {
+      explaination.push(err.message);
+    });
+
+    this.name = 'ValidationError';
+    this.message =message;
+    this.explaination =explaination;
+    this.statusCode =StatusCodes.BAD_REQUEST
+
+  }
+}
+
+module.exports = ValidationError;
